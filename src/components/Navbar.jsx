cd6d5dc521458ae1d5cae2b6d7df7689ed6ca162
thanhtungtd <thanhtunggithub@gmail.com>
2024-02-28 16:58:28 +0700

    login logout with firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+
+    const { currentUser } = useContext(AuthContext);
+    console.log(currentUser)
+
     return (
         <div className='navbar'>
             <span className='title'>Fun Chat</span>
             <div className='user'>
-                <img src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg" alt="image" />
-                <span className="user_name">my name</span>
+                <img src={currentUser.photoURL} alt="image" />
+                <span className="user_name">{currentUser.displayName}</span>
                 <button className='button' onClick={() => signOut(auth)} ><i className="fa fa-sign-out"></i></button>
             </div>
         </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { collection, query, where } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Search = () => {
+
+    const [userName, setUserName] = useState("");
+    const [user, setUer] = useState(null);
+    const [err, setErr] = useState(false);
+
+    const handleSearch = () => {
+        const q = query(collection(db, "users"))
+    }
+
+    const handleKey = e => {
+        e.code === "Enter" && handleSearch();
+    }
+
     return (
-        <div className='search'>
-            <div className='searchform'>
-                <input type="text" className='seek' placeholder='Find a user' />
+        <div className='search' >
+            <div className='searchform items-center'>
+                <input type="text" className='seek' placeholder='Find a user🔍' onKeyDown={handleKey} onChange={(e) => setUserName(e.target.value)} />
             </div>
             <div className='userChat'>
                 <img src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg" alt="" />
                 <div className='userInfo'>
                     <span>Name</span>
                 </div>
-
             </div>
-        </div>
+        </div >
     );
 };
 
